604559a5183166658917bd3a701a2aad4762f87c
jlooper <jen.looper@gmail.com>
2025-09-21 14:10:04 -0400

    editing config for netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 import tailwind from '@astrojs/tailwind';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
+  adapter: netlify(),
 });
